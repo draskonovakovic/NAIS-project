ac535ca6a7a5c664014972aa9c56b413fbe349b7
Pekseto <lukazecevic5@gmail.com>
2024-05-29 00:05:42 +0200

    added models, and changed port for graph service to 9051

---
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/model/Match.java
@@ -0,0 +1,25 @@
+package rs.ac.uns.acs.nais.GraphDatabaseService.model;
+
+import lombok.Data;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Node
+public class Match {
+    @Id
+    private Long id;
+
+    private LocalDateTime matchDay;
+    private String city;
+
+    @Relationship(value = "PLAYS_MATCH", direction = Relationship.Direction.OUTGOING)
+    private List<PlaysMatch> teams = new ArrayList<>();
+
+    private Boolean isActive;
+}
---
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/model/PlaysMatch.java
@@ -0,0 +1,14 @@
+package rs.ac.uns.acs.nais.GraphDatabaseService.model;
+
+import org.springframework.data.neo4j.core.schema.RelationshipId;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+@RelationshipProperties
+public class PlaysMatch {
+
+    @RelationshipId
+    private Long id;
+    @TargetNode
+    private Team team;
+}
