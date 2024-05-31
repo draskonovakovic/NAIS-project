a88eed34f816d5d48e293c7a80af0c4dafebed8f
Luka Todorovic <86576899+ToD0R@users.noreply.github.com>
2024-06-01 01:09:09 +0200

    fix: Some fixes on export pdf function

--- GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/service/impl/MatchService.java
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/service/impl/MatchService.java
@@ -1,9 +1,6 @@
 package rs.ac.uns.acs.nais.GraphDatabaseService.service.impl;
 
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.Paragraph;
+import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -81,26 +78,38 @@ public class MatchService implements IMatchService {
 
         Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 24, Font.BOLD);
 
-        Paragraph title = new Paragraph("REFEREE FOR RECOMMENDATION", titleFont);
+        Paragraph title = new Paragraph("MATCH NUMBER No." + matchFromDb.getId(), titleFont);
         title.setAlignment(Element.ALIGN_CENTER);
         document.add(title);
+
+        Font matchDataFont = FontFactory.getFont(FontFactory.HELVETICA, 16, Font.NORMAL);
+        Paragraph matchData = new Paragraph();
+        matchData.setAlignment(Element.ALIGN_CENTER);
+        matchData.add(new Chunk("City: " + matchFromDb.getCity() + "\n", matchDataFont));
+        matchData.add(new Chunk("Date: " + matchFromDb.getMatchDay().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss")) + "\n\n", matchDataFont));
+        document.add(matchData);
 
         Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12, Font.BOLD);
         PdfPCell headerCell1 = new PdfPCell(new Paragraph("First name", headerFont));
         PdfPCell headerCell2 = new PdfPCell(new Paragraph("Last name", headerFont));
         PdfPCell headerCell3 = new PdfPCell(new Paragraph("Contact", headerFont));
+        PdfPCell headerCell4 = new PdfPCell(new Paragraph("Recommended role", headerFont));
 
-        PdfPTable reportTable = new PdfPTable(3);
-        reportTable.setWidthPercentage(80);
+        PdfPTable reportTable = new PdfPTable(4);
+        reportTable.setWidthPercentage(90);
 
         reportTable.addCell(headerCell1);
         reportTable.addCell(headerCell2);
         reportTable.addCell(headerCell3);
+        reportTable.addCell(headerCell4);
 
+        int rank = 1;
         for(var referee: recommendationForMatch) {
             reportTable.addCell(referee.getName());
             reportTable.addCell(referee.getSurname());
             reportTable.addCell(referee.getEmail());
+            reportTable.addCell(GetRole(rank));
+            rank++;
         }
 
         document.add(reportTable);
@@ -109,4 +118,13 @@ public class MatchService implements IMatchService {
         return byteArrayOutputStream.toByteArray();
     }
 
+    private String GetRole(int rank) {
+        return switch (rank) {
+            case 1 -> "Main referee";
+            case 2 -> "Second referee";
+            case 3 -> "Third referee";
+            case 4 -> "Fourth referee";
+            default -> "Invalid Rank";
+        };
+    }
 }
